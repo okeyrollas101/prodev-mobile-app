3420c9c1944f84e387ffe2a17f7b3813417da388
Mac <mac@okey101.local>
2025-09-05 17:05:10 +0100

    Update layout.tsx

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,12 +1,6 @@
 import { Stack } from "expo-router";
-import { View } from "react-native";
+import '@/styles/global.css'
 
 export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="/" />
-      <Stack.Screen name="join" />
-      <Stack.Screen name="signin" />
-    </Stack>
-  );
+  return <Stack />;
 }
